4ee847d39bc199abba1e4be63f275a622ed8885d
Johannes Krupp <johannes.krupp@cispa.saarland>
2017-11-22 11:03:01 +0100

    memory-limit for plotting

--- plot_cfg.py
+++ plot_cfg.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python2.7
+import resource
 import sys
 
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 import ethanalyze.project
 
@@ -10,5 +11,9 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print 'Usage: %s <code>' % sys.argv[0]
         exit(-1)
+
+    
+    mem_limit = 4*1024*1024*1024 # 4GB
+    resource.setrlimit(resource.RLIMIT_AS, (mem_limit, mem_limit))
     p = ethanalyze.project.load(sys.argv[1])
     print p.cfg.to_dot()
